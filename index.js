1fc6a146ceb6d5564425412dbb8355c7dcbb30ed
JRnihar <niharmonirulislam@gmail.com>
2022-06-12 22:50:16 +0600

    final-commit

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-// const uri = `mongodb+srv://task-assigment:dt60p5L85A0Lfi2J@cluster0.dctmt.mongodb.net/?retryWrites=true&w=majority`;
-
-
-
 const uri = "mongodb+srv://task-assignment:dt60p5L85A0Lfi2J@cluster0.dctmt.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -24,11 +20,27 @@ async function run() {
     try {
         await client.connect();
         const personalCollection = client.db('Task').collection('personal')
-
+        const BusinessCollection = client.db('Task').collection('Business')
+        const LoanCollection = client.db('Task').collection('Loan')
+        //Personal-Details
         app.post('/review', async (req, res) => {
             const newProduct = req.body
-            console.log(newProduct);
+            // console.log(newProduct);
             const result = await personalCollection.insertOne(newProduct)
+            res.send(result)
+        })  
+        //Business-Details
+        app.post('/Business', async (req, res) => {
+            const newProduct = req.body
+            // console.log(newProduct);
+            const result = await BusinessCollection.insertOne(newProduct)
+            res.send(result)
+        })  
+        //Loan-Details
+        app.post('/loan', async (req, res) => {
+            const newProduct = req.body
+            console.log(newProduct);
+            const result = await LoanCollection.insertOne(newProduct)
             res.send(result)
         })  
 
